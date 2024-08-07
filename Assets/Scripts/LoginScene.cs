f779662f3257af816356935afbef574aeccc42d6
alfredo1995 <alfredogomes1995@gmail.com>
2024-08-07 03:12:07 -0300

    register user

---
+++ Assets/Scripts/LoginScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Project.Networking;
+using TMPro;
+
+namespace Project.Scenes
+{
+    public class LoginScene : MonoBehaviour
+    {
+        [SerializeField] TMP_InputField username, password;
+        public void RegisterUser() 
+        {
+            NetworkClient.getInstance.RegisterUser(username.text, password.text);
+        }
+    }
+}
--- Assets/Scripts/NetworkClient.cs
+++ Assets/Scripts/NetworkClient.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PlayerIOClient;
 
 namespace Project.Networking
 {
     public class NetworkClient : MonoBehaviour
     {
         public static NetworkClient mInstance;
+
+        public static string gameId = "poker-ju7fxknktumjiu4unpes4g";
 
         public static NetworkClient getInstance
         {
@@ -20,6 +23,23 @@ namespace Project.Networking
                 return mInstance;
             }
         }
+
+        public void RegisterUser (string username, string password)
+        {
+            PlayerIO.Authenticate(gameId, "public", new Dictionary<string, string>
+            {
+                {"register", "true"},
+                {"username", username},
+                {"password", password}
+            }, null, delegate (Client client)
+            {
+                Debug.Log("Registration successfull");
+            }, delegate (PlayerIOError error)
+            {
+                Debug.Log("Registration falied : " + error.Message);
+            });
+        }
     }
+
 }
 
